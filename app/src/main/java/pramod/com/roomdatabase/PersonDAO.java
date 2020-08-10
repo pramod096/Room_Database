f3979adb036e27007e1fec82c2d4b378f0a10afa
Pramod Reddy Gonegari <pramodreddy096@gmail.com>
2020-08-10 18:51:07 +0530

    Added Manipulation methods for Dao & Created DatabasePerson abstract class

--- app/src/main/java/pramod/com/roomdatabase/PersonDAO.java
+++ app/src/main/java/pramod/com/roomdatabase/PersonDAO.java
@@ -1,12 +1,26 @@
 package pramod.com.roomdatabase;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
 
 @Dao
 public interface PersonDAO {
 
     @Insert
     void insert(Person p);
+
+    @Query("select * from person")
+    List<Person> getAllData();
+
+    @Delete
+    void delete(Person p);
+
+    @Update
+    void update(Person p);
 
 }
---
+++ app/src/main/java/pramod/com/roomdatabase/PersonDatabase.java
@@ -0,0 +1,9 @@
+package pramod.com.roomdatabase;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Person.class}, version = 1, exportSchema = false)
+public abstract class PersonDatabase  extends RoomDatabase {
+
+}
