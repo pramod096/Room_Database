83f8fc6c1548f5d6e1b7a6ac48e58bfddd88cce3
Pramod Reddy Gonegari <pramodreddy096@gmail.com>
2020-08-10 18:36:11 +0530

    Added Entity

--- app/src/main/java/pramod/com/roomdatabase/Person.java
+++ app/src/main/java/pramod/com/roomdatabase/Person.java
@@ -1,4 +1,42 @@
 package pramod.com.roomdatabase;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "person")
 public class Person {
+    @PrimaryKey(autoGenerate = true)
+    int id;
+
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
